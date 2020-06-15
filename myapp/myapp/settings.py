1824d74d435bc56eaacacde7bd4302df4bd23cc3
nemo-CN <1042258591@qq.com>
2020-06-15 17:52:04 +0800

    GOGOGO

--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -56,7 +56,8 @@ ROOT_URLCONF = 'myapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["templates"],
+        # 'DIRS': [],
+        'DIRS':[os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
